d0696c4f700b0f8b8efd3dddc7290612183639e5
laiba130 <laiba2021@gmail.com>
2023-11-20 15:42:36 +0500

    Adding calculator to my repo

---
+++ calculator/cal03.py
@@ -0,0 +1,22 @@
+def Add(num1, num2):
+    return num1+num2
+
+def Sub(num1, num2):
+    return num1-num2
+
+def Mul(num1, num2):
+    return num1*num2
+
+def Div(num1, num2):
+    return num1/num2
+
+def SqRoot(num1, num2):
+    return num1**num2
+
+
+print(Add(1, 2))
+print(Sub(4,3))
+print(Mul(5, 6))
+print(Div(10, 2))
+print(SqRoot(2, 4))
+
